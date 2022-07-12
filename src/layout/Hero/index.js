b2ea93393a99db58c549e3604f02bff3a77e9ea8
wabloan <wabloan@wabloan.com>
2022-07-12 16:36:06 +0100

    ok

--- src/layout/Hero/index.js
+++ src/layout/Hero/index.js
@@ -3,7 +3,17 @@ import '../../assets/css/Home/hero.css'
 const Index = ()=>{
     return (
         <div className="Hero">
-            
+            <div className='nav'>
+                <div className='logoDiv'>
+                        LOGOG
+                </div>
+                <div className='navItems'>
+                    Wallet
+                </div>
+                <div className='authButtons'>
+                    <button>Login</button>
+                </div>
+            </div>
         </div>
     )
 }
