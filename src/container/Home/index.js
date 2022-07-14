a2d32a0093fc474e06fb2a5ec8767c6798ef9fbb
wabloan <wabloan@wabloan.com>
2022-07-14 08:34:25 +0100

    okay

--- src/container/Home/index.js
+++ src/container/Home/index.js
@@ -1,11 +1,12 @@
 import Hero from '../../layout/Hero'
 import Section1 from '../../layout/Section1'
-
+import Section2 from '../../layout/Section2'
 const Index = ()=>{
     return (
         <div className="Home">
             <Hero/>
             <Section1/>
+            <Section2/>
         </div>
     )
 }
---
+++ src/layout/Section2/index.js
@@ -0,0 +1,10 @@
+import '../../assets/css/Home/section2.css'
+
+const Index = ()=>{
+    return (
+        <div className="section2">
+            <h1>Tenacity</h1>
+        </div>
+    )
+}
+export default Index;
