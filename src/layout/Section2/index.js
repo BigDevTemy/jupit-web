6a4e3bf1a331de4af59f6b1c7f230de297d9aac4
wabloan <wabloan@wabloan.com>
2022-07-14 17:12:44 +0100

    okay

--- src/layout/Section2/index.js
+++ src/layout/Section2/index.js
@@ -1,5 +1,8 @@
 import '../../assets/css/Home/section2.css'
 import icon1 from '../../assets/images/icons/Group.png'
+import icon2 from '../../assets/images/icons/award.png'
+import icon3 from '../../assets/images/icons/trade.png'
+import icon4 from '../../assets/images/icons/user.png'
 const Index = ()=>{
     return (
         <div className="section2">
@@ -20,16 +23,52 @@ const Index = ()=>{
                                     <img src={icon1}/>
                                 </div>
                                 <div>
-                                    <div>Secure storage</div>
-                                    <div>
+                                    <div className='securestorage'>Secure storage</div>
+                                    <div className='We_store'>
                                         We store the vast majority of the digital assets in secure offline storage.
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div>Odewumi</div>
-                        <div>Vincent</div>
-                        <div>Ademilola</div>
+                        <div>
+                            <div className='side'>
+                                <div>
+                                    <img src={icon4}/>
+                                </div>
+                                <div>
+                                    <div className='securestorage'>Protected by insurance</div>
+                                    <div className='We_store'>
+                                      Cryptocurrency stored on our servers is covered by our insurance policy.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                        <div className='side'>
+                                <div>
+                                    <img src={icon3}/>
+                                </div>
+                                <div>
+                                    <div className='securestorage'>Industry best practices</div>
+                                    <div className='We_store'>
+                                    Besnik crypto supports a variety of the most popular digital crypto currencies.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                        <div className='side'>
+                                <div>
+                                    <img src={icon4}/>
+                                </div>
+                                <div>
+                                    <div className='securestorage'>Trade Assets</div>
+                                    <div className='We_store'>
+                                    Discover new and innovative crypto assets with over 200 spot trading pairs and 25 margin.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     
             </div>
