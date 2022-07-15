d83d8d6b15fd2c223f7930e29eda59f0fbf15164
wabloan <wabloan@wabloan.com>
2022-07-15 08:56:12 +0100

    okay

--- src/layout/Section4/index.js
+++ src/layout/Section4/index.js
@@ -6,8 +6,11 @@ const Index = ()=>{
     return (
         <div className="section4">
             <div className='card'>
-                <div className='easiest'>The Easiest and Most</div>
-                <div className='quote'><img src={quote}/></div>
+                <div className='testimonia'>
+                    <div className='easiest'>The Easiest and Most</div>
+                    <div className='quote'><img src={quote} className="quoteImg"/></div>
+                </div>
+                
             </div>
         </div>
     )
