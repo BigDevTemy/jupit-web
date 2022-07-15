eba4e8469358d16fe5f0c5cc2d2518b90f931b16
wabloan <wabloan@wabloan.com>
2022-07-15 09:28:27 +0100

    ok

--- src/layout/Section4/index.js
+++ src/layout/Section4/index.js
@@ -1,6 +1,9 @@
 
 import '../../assets/css/Home/section4.css'
-import quote from '../../assets/images/2x.png'
+import quote from '../../assets/images/2x.png';
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
 
 const Index = ()=>{
     return (
@@ -9,7 +12,13 @@ const Index = ()=>{
                 <div className='testimonia'>
                     <div className='easiest'>The Easiest and Most</div>
                     <div className='quote'><img src={quote} className="quoteImg"/></div>
+                   
                 </div>
+                <div className='testimoniaComment'>
+                    
+
+                </div>
+                
                 
             </div>
         </div>
