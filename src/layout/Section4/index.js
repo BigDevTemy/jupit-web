19746487bb10668725626a74a4174fb2f573204b
wabloan <wabloan@wabloan.com>
2022-07-15 08:30:59 +0100

    okay

--- src/layout/Section4/index.js
+++ src/layout/Section4/index.js
@@ -1,7 +1,14 @@
+
+import '../../assets/css/Home/section4.css'
+import quote from '../../assets/images/2x.png'
+
 const Index = ()=>{
     return (
-        <div>
-            Welcome
+        <div className="section4">
+            <div className='card'>
+                <div>The Easiest and Most</div>
+                <div><img src={quote}/></div>
+            </div>
         </div>
     )
 }
