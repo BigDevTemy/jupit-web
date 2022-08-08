2a40921e78ced6078a52b61b5163de746e121f98
wabloan <wabloan@wabloan.com>
2022-08-08 17:48:44 +0100

    ok

--- src/layout/Hero/index.js
+++ src/layout/Hero/index.js
@@ -1,12 +1,29 @@
 import '../../assets/css/Home/hero.css'
-import logo from '../../assets/images/jupit.svg'
+import logo from '../../assets/images/jupit_white.svg'
 import Phone from '../../assets/images/phone1.png'
 import banner from '../../assets/images/banner.svg'
+
 const Index = ()=>{
     return (
         <div className="Hero">
            <img src={banner} />
-            
+            <div className='navTab'>
+                   <div className='Tab'>
+                        <div>
+                            <img src={logo} className="imgLogo"/>
+                        </div>
+                        <div className='navItems'>
+                            <div><button className='btn btn-md feature'>Feature</button></div>
+                            <div>How it Works</div>
+                            <div>Benefit</div>
+                            <div>About us</div>
+                            <div>Who we are</div>
+                        </div>
+                        <div>
+                            <div>Feature</div>
+                        </div>
+                   </div>
+            </div>
         </div>
     )
 }
