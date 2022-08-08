34e7c9df6410d0cf87ce1c6e3ef61cebf0ad9464
wabloan <wabloan@wabloan.com>
2022-08-08 20:19:03 +0100

    ok

--- src/layout/Hero/index.js
+++ src/layout/Hero/index.js
@@ -2,6 +2,7 @@ import '../../assets/css/Home/hero.css'
 import logo from '../../assets/images/jupit_white.svg'
 import Phone from '../../assets/images/phone1.png'
 import banner from '../../assets/images/banner.svg'
+import umbrella from '../../assets/images/umbrella.svg'
 
 const Index = ()=>{
     return (
@@ -25,6 +26,19 @@ const Index = ()=>{
                                 Register
                             </button>
                         </div>
+                   </div>
+                   <div className='heroContent'>
+                        <div className='contentHero'>
+                            <div>Digital currency leads at market in the right amount.</div>
+                            <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</div>
+                        </div>
+                        <div>
+                            <img src={umbrella}/>
+                        </div>
+                   </div>
+                   <div className='heroButton'>
+                            <div>Get Started</div>
+                            <div>View more</div>
                    </div>
             </div>
         </div>
