7dfa7f17250068d4cc5a6577d9c51f13585c2d7c
wabloan <wabloan@wabloan.com>
2022-08-08 21:39:03 +0100

    ok

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -1,8 +1,19 @@
 import '../../assets/css/Footer/footer.css'
+import backgroundFooter from '../../assets/images/BackgroundFooter.svg'
 const Index= ()=>{
     return (
         <div className="my_footer">
+            <div className='footerBg'>
+                <img src={backgroundFooter}/>
+            </div>
+            <div className='footerContent'>
+                    <div className='userCount'>12,500,601 </div>
+                    <div className='userText'>Users & Still Counting</div>
 
+                    <div className='whatareyou'>What are you waiting for?</div>
+                    <div className='footerBtn'>Get Started</div>
+            </div>
+            
         </div>
     )
 }
