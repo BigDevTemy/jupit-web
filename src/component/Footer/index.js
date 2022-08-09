e65b63d20788d4c5f3baa9a4af605055d99fc94b
wabloan <wabloan@wabloan.com>
2022-08-09 10:56:50 +0100

    ok

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -11,29 +11,29 @@ const Index= ()=>{
                 <div className='whatareyou'>What are you waiting for?</div>
                 <div className='footerBtn'>Get Started</div>
                 <div className='footerInfor'>
-                <div>
-                        <div><img src={footerLogo}/></div>
-                        <small>..your fast and reliable exchange</small>
+                    <div>
+                            <div><img src={footerLogo}/></div>
+                            <small>..your fast and reliable exchange</small>
 
-                        <div className='quickLinkDiv'>Quick Links</div>
-                        <div className='quickLink'>
-                            <div>About Us</div>
-                            <div>Faq</div>
-                            <div>Who we are</div>
-                            <div>Our Team</div>
-                            <div>Our Products</div>
-                            <div>Our Mission</div>
-                        
+                            <div className='quickLinkDiv'>Quick Links</div>
+                            <div className='quickLink'>
+                                <div>About Us</div>
+                                <div>Faq</div>
+                                <div>Who we are</div>
+                                <div>Our Team</div>
+                                <div>Our Products</div>
+                                <div>Our Mission</div>
                             
-                        </div>
+                                
+                            </div>
 
-                </div>
-                <div>
-                        <h1>Blog POst</h1>
-                </div>
-                <div>
-                    <h1>Social Media</h1>
-                </div>
+                    </div>
+                    <div>
+                            <h1>Blog POst</h1>
+                    </div>
+                    <div>
+                        <h1>Social Media</h1>
+                    </div>
             </div>
             </div>
            
