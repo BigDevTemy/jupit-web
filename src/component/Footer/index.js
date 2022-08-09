3a9785560e7b85ec9407b2a732f5ffaeb4d4f5e8
wabloan <wabloan@wabloan.com>
2022-08-09 17:34:25 +0100

    okay

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -1,6 +1,13 @@
 import '../../assets/css/Footer/footer.css'
 import backgroundFooter from '../../assets/images/BackgroundFooter.svg'
 import footerLogo from '../../assets/images/footerlogo.svg'
+import blogpost1 from '../../assets/images/blogpost1.png'
+import blogpost2 from '../../assets/images/blogpost2.png'
+import blogpost3 from '../../assets/images/blogpost3.png'
+import twitter from '../../assets/images/twitter.png'
+import instagram from '../../assets/images/instagram.png'
+import linkedin from '../../assets/images/linkedin.png'
+import facebook from '../../assets/images/facebook.png'
 const Index= ()=>{
     return (
         <div className="my_footer">
@@ -10,31 +17,14 @@ const Index= ()=>{
 
                 <div className='whatareyou'>What are you waiting for?</div>
                 <div className='footerBtn'>Get Started</div>
+
                 <div className='footerInfor'>
-                    <div>
-                            <div><img src={footerLogo}/></div>
-                            <small>..your fast and reliable exchange</small>
-
-                            <div className='quickLinkDiv'>Quick Links</div>
-                            <div className='quickLink'>
-                                <div>About Us</div>
-                                <div>Faq</div>
-                                <div>Who we are</div>
-                                <div>Our Team</div>
-                                <div>Our Products</div>
-                                <div>Our Mission</div>
-                            
-                                
-                            </div>
-
-                    </div>
-                    <div>
-                            <h1>Blog POst</h1>
-                    </div>
-                    <div>
-                        <h1>Social Media</h1>
-                    </div>
-            </div>
+                        <div>1</div>
+                        <div>2</div>
+                        <div>3</div>
+                </div>
+                   
+            
             </div>
            
             
