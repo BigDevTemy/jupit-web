7d4b671a377e571b1ff3ead400e9cfa2751c64a3
wabloan <wabloan@wabloan.com>
2022-08-16 17:16:42 +0100

    ok

--- src/layout/Otclayout/index.js
+++ src/layout/Otclayout/index.js
@@ -5,6 +5,8 @@ import feature6 from '../../assets/images/feature-6.png'
 import feature2 from '../../assets/images/feature-2.png'
 import feature7 from '../../assets/images/feature-7.png'
 import feature3 from '../../assets/images/feature-3.png'
+import serviceimage from '../../assets/images/service-image-1.png'
+import {FaArrowRight} from 'react-icons/fa'
 const Index = ()=>{
     return (
         <div>
@@ -89,6 +91,62 @@ const Index = ()=>{
                                 </div>
                         </div>
                     </div>
+                    <div className='tradeflow'>
+                       <div>
+                            <img src={serviceimage}/>
+
+                       </div>
+                       <div>
+                            <div className='ourtradeflow'>Our OTC Trade Flow</div>
+                            <div className='signup'>
+                                <div>
+                                    <FaArrowRight color='#3042c9'/>
+                                </div>
+                                <div>
+                                    <div className='signupTitle'>Signup</div>
+                                    <div  className='signupmessage'>Get started by filling out the OTC registration form or by simply emailing otc@jupit.com with your inquiry.</div>
+                                </div>
+                            </div>
+                            <div className='signup'>
+                                <div>
+                                    <FaArrowRight color='#3042c9'/>
+                                </div>
+                                <div>
+                                    <div className='signupTitle'>Onboarding</div>
+                                    <div className='signupmessage'>To become an OTC client with us, it is important to go through the standard onboarding process obtaining the highest level of verification by providing documents such as a Government ID, Proof of Residence and Proof of Wealth to enable large transactions.</div>
+                                </div>
+                            </div>
+                            <div className='signup'>
+                                <div>
+                                    <FaArrowRight color='#3042c9'/>
+                                </div>
+                                <div>
+                                    <div className='signupTitle'>Communication</div>
+                                    <div  className='signupmessage'>A private Chat Group for trade will be setup either by WhatsApp or Signal Messaging App and our OTC agents will walk you through your bespoke experience.</div>
+                                </div>
+                            </div>
+                            <div className='signup'>
+                                <div>
+                                    <FaArrowRight color='#3042c9'/>
+                                </div>
+                                <div>
+                                    <div className='signupTitle'>Open Trade</div>
+                                    <div  className='signupmessage'>Request a quote for your trade in the private chat group to start executing your orders. Your trade agent will be available at all time to provide you with any support you may need.</div>
+                                </div>
+                            </div>
+                            <div className='signup'>
+                                <div>
+                                    <FaArrowRight color='#3042c9'/>
+                                </div>
+                                <div>
+                                    <div className='signupTitle'>Settlement of Fiat/Crypto</div>
+                                    <div  className='signupmessage'>Once the OTC Desk receives the payment confirmation for your order, our agent will send the settlement asset or fiat to the specified wallet/account, along with a confirmation email to your inbox, and the trade is complete.</div>
+                                </div>
+                            </div>
+
+                       </div>
+                    </div>
+                    
             </div>
             
         </div>
