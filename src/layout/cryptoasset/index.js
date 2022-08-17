5d7094e1916020a906e5c26e5999246b0b6965a9
wabloan <wabloan@wabloan.com>
2022-08-17 17:01:10 +0100

    okay

--- src/layout/cryptoasset/index.js
+++ src/layout/cryptoasset/index.js
@@ -8,13 +8,22 @@ import happyclient from '../../assets/images/happy-client.png'
 import account from '../../assets/images/account.png'
 import bank from '../../assets/images/bank.png'
 import buy from '../../assets/images/buy.png'
+import Hodl from '../../assets/images/hodl.png'
+import datatransfer from '../../assets/images/data-transfer.png'
+import buy1 from '../../assets/images/buy-1.png'
 import {TiArrowRightThick} from 'react-icons/ti'
 import {FiCheckCircle} from 'react-icons/fi'
+import Aos from 'aos';
+import 'aos/dist/aos.css'
+import { useEffect } from 'react'
 const Index = ()=>{
+    useEffect(()=>{
+        Aos.init({duration:2000})
+},[])
     return (
         <div className="crypto-asset">
-            <div className='buynsell'>
-                    <div>
+            <div className='buynsell' >
+                    <div data-aos="fade-up">
                         <div className='tradewithjupitTitle'>Buy and Sell Crypto in minutes.</div>
                         <div className='tradewithjupit'>
                             Trade with Jupit and experience one of the most convenient, fastest and secure way to sell your crypto. It takes just 120 seconds!
@@ -24,7 +33,7 @@ const Index = ()=>{
                         </div>
 
                     </div>
-                    <div>
+                    <div data-aos="fade-up">
                         <img src={buynsell}/>
                     </div>
             </div>
@@ -32,26 +41,26 @@ const Index = ()=>{
                     <div className='mosttrusted'>The most trusted crypto exchange platform</div>
                     <div className='smallTalk'>Here are a few reasons why you should choose Jupit</div>
                     <div className='whychooseus'>
-                        <div>
+                        <div data-aos="fade-up">
                             <div className='transaction-img'><img src={fast}/></div>
                             <div className='transaction'>Fast Transaction</div>
                             <div className='transaction-text'>Fast and easy-to-use trading services in one integrated platform.</div>
                         </div>
-                        <div>
+                        <div data-aos="fade-down">
                             <div className='transaction-img'><img src={secure}/></div>
                             <div className='transaction'>Secure Transaction</div>
                             <div className='transaction-text'>
                                 You can rest easy knowing that your crypto is safe with us anytime!
                             </div>
                         </div>
-                        <div>
+                        <div data-aos="fade-up">
                             <div className='transaction-img'><img src={customer}/></div>
                             <div className='transaction'>Customer First</div>
                             <div className='transaction-text'>
                                 You can rest easy knowing that your crypto is safe with us anytime!
                             </div>
                         </div>
-                        <div>
+                        <div data-aos="fade-down">
                             <div className='transaction-img'><img src={happyclient}/></div>
                             <div className='transaction'>Built For You</div>
                             <div className='transaction-text'>
@@ -63,34 +72,34 @@ const Index = ()=>{
 
             <div class="your-crypto">
                 <div className='laptopDiv'>
-                    <div className='mockup'></div>
+                    <div className='mockup' data-aos="fade-left"></div>
                     <div>
                         <div className='spotlight'>
-                            <div className='SportlightDiv'>
+                            <div className='SportlightDiv' data-aos="fade-up">
                                 <div className='check'><FiCheckCircle size={30} color="#4BB543"/></div>
                                 <div className='Yourcrypto'>Your crypto on-the-go</div>
                             </div>
-                            <div className='YourcryptoText'>
+                            <div className='YourcryptoText' data-aos="fade-up" >
                                 Manage your wallet easily anywhere, anytime, and from any device.
                             </div>
                         </div>
 
-                        <div className='spotlight'>
-                            <div className='SportlightDiv'>
+                        <div className='spotlight' >
+                            <div className='SportlightDiv' data-aos="fade-up">
                                 <div className='check'><FiCheckCircle size={30} color="#4BB543"/></div>
                                 <div className='Yourcrypto'>You are in control</div>
                             </div>
-                            <div className='YourcryptoText'>
+                            <div className='YourcryptoText' data-aos="fade-up" >
                             Have complete control of your funds on our secured platform.
                             </div>
                         </div>
 
-                        <div className='spotlight'>
-                            <div className='SportlightDiv'>
+                        <div className='spotlight' >
+                            <div className='SportlightDiv' data-aos="fade-up">
                                 <div className='check'><FiCheckCircle size={30} color="#4BB543"/></div>
                                 <div className='Yourcrypto'>Move Beyond borders</div>
                             </div>
-                            <div className='YourcryptoText'>
+                            <div className='YourcryptoText' data-aos="fade-up">
                                 Complete your payments around the world instantly with your asset on Jupit.
                             </div>
                         </div>
@@ -104,20 +113,20 @@ const Index = ()=>{
             <div className='whatyoucando'>
                     <div className='whatyoucando_title'>What can you do  with your Jupit Account?</div>
                     <div className='whychooseus'>
-                        <div>
-                            <div className='transaction-img'><img src={fast}/></div>
+                        <div data-aos="fade-up">
+                            <div className='transaction-img' data-aos="flip-right"><img src={buy1}/></div>
                             <div className='transaction'>Sell And Buy</div>
                             <div className='transaction-text'>Sell and buy at the best rate and cash out fiat to your bank account instantly.</div>
                         </div>
-                        <div>
-                            <div className='transaction-img'><img src={secure}/></div>
+                        <div data-aos="fade-up">
+                            <div className='transaction-img' data-aos="flip-right"><img src={datatransfer}/></div>
                             <div className='transaction'>Send and Receive</div>
                             <div className='transaction-text'>
                                 Transfer or receive assets with ease, no matter where you are or who you are with.
                             </div>
                         </div>
-                        <div>
-                            <div className='transaction-img'><img src={customer}/></div>
+                        <div  data-aos="fade-up">
+                            <div className='transaction-img' data-aos="flip-right"><img src={Hodl}/></div>
                             <div className='transaction'>Hodl</div>
                             <div className='transaction-text'>
                                 Hold your crypto assets safe and secure in your Jupit account indefinitely. We have you covered!
@@ -127,7 +136,7 @@ const Index = ()=>{
                     </div>
                     
                     <div className="whatyoucando_title getStarted">Get started in a few minutes</div>
-                    <div className='createAccount'>
+                    <div className='createAccount'  data-aos="fade-up">
                         <div>
                             <div>
                                 <img src={account} className="acctImage"/>
