bf1110db4875a17303df75c4fb16fcd8ff0fd63d
wabloan <wabloan@wabloan.com>
2022-08-24 13:48:05 +0100

    okay

--- src/layout/contactLayout/index.js
+++ src/layout/contactLayout/index.js
@@ -8,6 +8,8 @@ const Index = ()=>{
                         <div className='iconHome'>
                             <IoHomeOutline  size="30" color='#4bb543' style={{cursor:'pointer'}}/>
                         </div>
+                        <div className='say_hello'>Say Hello to Us</div>
+                        <div className='question'>Do you have any questions? Are you ready to reduce cost and create new revenue? Or you simply want to say hi? Drop us a message.</div>
                     </div>
             </div>
         </div>
