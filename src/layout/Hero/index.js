54b95f168c3f2acdf6b492bd2a31495c2e779448
wabloan <wabloan@wabloan.com>
2022-08-24 11:28:29 +0100

    ok

--- src/layout/Hero/index.js
+++ src/layout/Hero/index.js
@@ -1,7 +1,7 @@
 import '../../assets/css/Home/hero.css'
 import logo from '../../assets/images/jupit_white.svg'
 import Phone from '../../assets/images/phone1.png'
-import banner from '../../assets/images/banner.svg'
+import banner from '../../assets/images/banner.png'
 import umbrella from '../../assets/images/umbrella.svg'
 import { Link } from 'react-router-dom'
 import { Link as ScrollLink } from 'react-scroll'
