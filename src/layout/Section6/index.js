516f7e9b3aa90f248452a93832bf72df7a220edd
wabloan <wabloan@wabloan.com>
2022-08-24 16:22:44 +0100

    okay

--- src/layout/Section6/index.js
+++ src/layout/Section6/index.js
@@ -1,28 +1,32 @@
 import '../../assets/css/Home/section6.css'
 const Index=()=>{
     return (
-        <div className="section6" id="contact">
+                <div className="section6" id="contact">
                    <div className='CryptoWallet'>Get in touch</div>
                    <div className='formDiv'>
-                    <div className='form'>
-                            <div className='form-group fm-group'>
-                                <input type="text" placeholder='Your name' className='form-control fg-input'/>
-                            </div>
-                            <div className='form-group fm-group'>
-                                <input type="text" placeholder='Your Email' className='form-control fg-input'/>
-                            </div>
-                            <div className='form-group fm-group'>
-                               <textarea className='form-control' rows="10" placeholder='Message'>
+                        <div className='form'>
+                                <div className='form-group fm-group'>
+                                    <input type="text" placeholder='Your name' className='form-control fg-input'/>
+                                </div>
+                                <div className='form-group fm-group'>
+                                    <input type="text" placeholder='Your Email' className='form-control fg-input'/>
+                                </div>
+                                <div className='form-group fm-group'>
+                                <textarea className='form-control' rows="10" placeholder='Message'>
 
-                               </textarea>
-                            </div>
-                            <div className='form-group'>
-                                <input type="button" value="Submit" className='btn btn-primary-me form-control' />
-                            </div>
+                                </textarea>
+                                </div>
+                                <div className='form-group'>
+                                    <input type="button" value="Submit" className='btn btn-primary-me form-control' />
+                                </div>
+
+                                <div>
+
+                                </div>
+                        </div>
                     </div>
-                   </div>
                    
-        </div>
+                </div>
     )
 }
 
--- src/layout/contactLayout/index.js
+++ src/layout/contactLayout/index.js
@@ -1,15 +1,75 @@
 import '../../assets/css/contact/contact.css'
 import {IoHomeOutline} from 'react-icons/io5'
+import {BsArrowLeft} from 'react-icons/bs'
+import TextField from '@mui/material/TextField';
+import { Link } from 'react-router-dom'
+import Aos from 'aos';
+import 'aos/dist/aos.css'
+import { useEffect } from 'react';
+import Button from '@mui/material/Button';
 const Index = ()=>{
+    useEffect(()=>{
+        Aos.init({duration:2000})
+},[])
     return (
         <div className="contactIndex">
             <div className='heroContact'>
                     <div className='cardContact'>
                         <div className='iconHome'>
-                            <IoHomeOutline  size="30" color='#4bb543' style={{cursor:'pointer'}}/>
+                            <div className='homeDiv'>
+                                <IoHomeOutline  size="30" color='#4bb543' style={{cursor:'pointer'}} data-aos="fade-up" />
+                            </div>
+                            <Link to='/' className='hide'>
+                                <div className='backDiv '>
+                                    <BsArrowLeft size="25" color="#4bb543" data-aos="fade-left"  data-aos-duration="4000"/>
+                                </div>
+                            </Link>
+                            
                         </div>
                         <div className='say_hello'>Say Hello to Us</div>
                         <div className='question'>Do you have any questions? Are you ready to reduce cost and create new revenue? Or you simply want to say hi? Drop us a message.</div>
+                        <div className='forms'>
+                            <div className='formDiv'>
+
+                                <div className='form-group'>
+                                    <TextField id="filled-basic" label="What is your full name ? *" variant="filled" className='form-control' />
+                                </div>
+                                <div className='form-group'>
+                                    <TextField required id="filled-basic" label="What is your email ? *" variant="filled" className='form-control' />
+                                </div>
+                            </div>
+
+                            <div className='formDiv'>
+
+                                <div className='form-group'>
+                                    <TextField id="filled-basic" label="what is your phone number ? *" variant="filled" className='form-control' />
+                                </div>
+                                <div className='form-group'>
+                                    <TextField id="filled-basic" label="what is your company ?" variant="filled" className='form-control' />
+                                </div>
+                            </div>
+                            <div className='formDiv'>
+
+                                <div className='form-group'>
+                                    <TextField id="filled-basic" 
+                                        label="write your message? *"
+                                        multiline
+                                        rows={5}
+                                        variant="filled" 
+                                        className='form-control' />
+                                </div>
+                                
+                            </div>
+                            <div className='formDiv'>
+
+                                <div className='form-group'>
+                                    <Button fullWidth variant="contained" style={{backgroundColor:'#1c1c93',height:50}}>Send Message</Button>
+                                </div>
+                                
+                            </div>
+
+                            
+                        </div>
                     </div>
             </div>
         </div>
