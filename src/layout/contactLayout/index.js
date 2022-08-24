e590545020466faafbcb3d5be4c73850bdae4f1b
wabloan <wabloan@wabloan.com>
2022-08-24 17:54:20 +0100

    ok

--- src/layout/contactLayout/index.js
+++ src/layout/contactLayout/index.js
@@ -7,13 +7,21 @@ import Aos from 'aos';
 import 'aos/dist/aos.css'
 import { useEffect } from 'react';
 import Button from '@mui/material/Button';
+import {RiWhatsappFill} from 'react-icons/ri'
 const Index = ()=>{
     useEffect(()=>{
         Aos.init({duration:2000})
 },[])
+const chat = ()=>{
+
+}
     return (
         <div className="contactIndex">
+            <div className='whatsappIcon'>
+                    <a href="https://wa.me/2348088213177" target="_blank"><RiWhatsappFill size={50} style={{cursor:'pointer'}} color="#25D366"  /></a>
+                </div>
             <div className='heroContact'>
+                
                     <div className='cardContact'>
                         <div className='iconHome'>
                             <div className='homeDiv'>
