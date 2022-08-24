6a7ea608d8626957592b0ae5e2fc5f1195d8e23b
wabloan <wabloan@wabloan.com>
2022-08-24 18:03:34 +0100

    ok

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -46,7 +46,7 @@ const Index = ({scroll})=>{
                         </div>
 
                     </div>
-                    <div className={scroll > 0 ? 'navLinkScroll product':'navLink product'}><Link to='/contact' style={{color:'#000'}}>Contact</Link></div>
+                    <div className={scroll > 0 ? 'navLinkScroll product':'navLink product'}><Link to='/contact' style={{textDecoration:'none'}}  className={scroll > 0 ? 'navLinkScroll product':'navLink product'}>Contact</Link></div>
                     
                     
                 </div>
