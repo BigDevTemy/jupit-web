656114f7377f92432592a8d18bc88bc0111a2da5
wabloan <wabloan@wabloan.com>
2022-10-24 11:53:44 +0100

    ok

--- src/layout/contactLayout/index.js
+++ src/layout/contactLayout/index.js
@@ -5,13 +5,27 @@ import TextField from '@mui/material/TextField';
 import { Link } from 'react-router-dom'
 import Aos from 'aos';
 import 'aos/dist/aos.css'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import {RiWhatsappFill} from 'react-icons/ri'
 const Index = ()=>{
     useEffect(()=>{
         Aos.init({duration:2000})
 },[])
+const [Form,setForm] = useState({})
+
+const handleChange = (e)=>{
+    const {name,value} = e.target
+    setForm({...Form,[name]:value});
+}
+
+const RegisterValidForm = !Form.fullname?.length 
+    || !Form.email?.length 
+    || !Form.password?.length 
+    || !Form.phonenumber?.length
+    || !Form.message?.length
+    
+    
 const chat = ()=>{
 
 }
@@ -37,23 +51,24 @@ const chat = ()=>{
                         <div className='say_hello'>Say Hello to Us</div>
                         <div className='question'>Do you have any question or simply want to say hi? Drop us a message.</div>
                         <div className='forms'>
+                            
                             <div className='formDiv'>
 
                                 <div className='form-group'>
-                                    <TextField id="filled-basic" label="Full name *" variant="filled" className='form-control' />
+                                    <TextField id="filled-basic" label="Full name *" onChange={handleChange} name='fullname' variant="filled" className='form-control' />
                                 </div>
                                 <div className='form-group'>
-                                    <TextField required id="filled-basic" label="Email" variant="filled" className='form-control' />
+                                    <TextField required id="filled-basic" label="Email" onChange={handleChange} name='email' variant="filled" className='form-control' />
                                 </div>
                             </div>
 
                             <div className='formDiv'>
 
                                 <div className='form-group'>
-                                    <TextField id="filled-basic" label="Phone number *" variant="filled" className='form-control' />
+                                    <TextField id="filled-basic" label="Phone number *" onChange={handleChange} name="phonenumber" variant="filled" className='form-control' />
                                 </div>
                                 <div className='form-group'>
-                                    <TextField id="filled-basic" label="Company" variant="filled" className='form-control' />
+                                    <TextField id="filled-basic" label="Company" onChange={handleChange} name="company" variant="filled" className='form-control' />
                                 </div>
                             </div>
                             <div className='formDiv'>
@@ -64,14 +79,14 @@ const chat = ()=>{
                                         multiline
                                         rows={5}
                                         variant="filled" 
-                                        className='form-control' />
+                                        className='form-control' name="message" onChange={handleChange} />
                                 </div>
                                 
                             </div>
                             <div className='formDiv'>
 
                                 <div className='form-group'>
-                                    <Button fullWidth variant="contained" style={{backgroundColor:'#1c1c93',height:50}}>Send Message</Button>
+                                    <Button fullWidth variant="contained" disabled={RegisterValidForm} style={{backgroundColor:'#1c1c93',height:50}} >Send Message</Button>
                                 </div>
                                 
                             </div>
